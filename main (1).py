8ea9c4fe5a02ca19d769d6fbee1473a578fb135b
mustafaalik <155713787+mustafaalik@users.noreply.github.com>
2024-01-24 06:19:57 +0500

    Add files via upload

---
+++ main (1).py
@@ -0,0 +1,28 @@
+# Task 1
+favorite_fruits = ['apple', 'banana', 'orange']
+favorite_fruits.append('grape')
+print(favorite_fruits)
+
+# Task 2
+del favorite_fruits[2]
+print(favorite_fruits)
+
+# Task 3
+list1 = [1, 2, 3]
+list2 = [4, 5, 6]
+concatenated_list = list1 + list2
+print(concatenated_list)
+
+# Task 4
+original_list = [2, 4, 6, 8]
+doubled_list = [x * 2 for x in original_list]
+print(doubled_list)
+
+# Task 5
+days_of_week = ('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday')
+third_day = days_of_week[2]
+print(third_day)
+
+# Task 6
+people_info = [('mustafa', 25), ('iis a', 30), ('nigga', 22)]
+print(people_info)
